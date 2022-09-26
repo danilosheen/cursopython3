ce0f7dd7c7ef15c1725ca85527958a9c92180c31
danilosheen <c.danilo.f.silva@gmail.com>
2022-09-26 01:12:57 -0300

    first day, curse of python

---
+++ logica_programacao/aula1.py
@@ -0,0 +1,16 @@
+#isso é um comentário de uma linha
+print('Linha1')
+print('Linha2')
+"""
+pode ser usado como comentário
+muito usado como documentação
+print('Linha1')
+print('Linha1')
+print('Linha1')
+"""
+print('Linha3')
+'''
+pode ser usado como comentário
+aspas duplas ou aspas simples são alternativas
+print('Linha4')
+'''
