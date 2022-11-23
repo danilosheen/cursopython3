50f4abf61df1160c28fccb4a9c698a461bde39dd
danilosheen <c.danilo.f.silva@gmail.com>
2022-11-23 13:35:38 -0300

    desafio lista de compras com index

---
+++ logica_programacao/exercicio_indexação.py
@@ -0,0 +1,3 @@
+lista = ['nome1', 'nome2', 'nome3', 'nome4']
+for i in range(len(lista)):
+    print(f'{i}, {lista[i]}')
---
+++ logica_programacao/exercicio_lista_de_compas.py
@@ -0,0 +1,54 @@
+from os import system
+import os
+import time
+
+
+lista = []
+while True:
+    print('Escolha uma opção:')
+    entrada = (input('(i)-Inserir \n(r)-Remover \n(l)-Listar \n(s)-Sair \n').lower())
+    if entrada == 'i':
+        valor = input('Insira um valor: ')
+        lista.append(valor)
+        print('Valor inserido!')
+        time.sleep(1)
+        os.system('clear')
+
+    elif entrada == 'r':
+        try:
+            indice = int(input('Informe o indice: '))
+            lista.pop(indice)
+            os.system('clear')
+
+        except Exception as error:
+            print(error)
+            os.system('clear')
+            time.sleep(1)
+            continue
+        
+
+    elif entrada == 'l':
+        if len(lista) > 0:
+            os.system('clear')
+            for indice, valor in enumerate(lista):
+                print(f'Indice:{indice}, Valor: {valor}')
+            time.sleep(1)
+                
+            
+        else:
+            print('A lista está vazia!')
+            time.sleep(1)
+            os.system('clear')
+            continue
+
+    elif entrada == 's':
+        os.system('clear')
+        print('Saindo...')
+        time.sleep(2)
+        break
+
+    else:
+        os.system('clear')
+        print('Valor digitado incorreto')
+        time.sleep(1)
+        continue
