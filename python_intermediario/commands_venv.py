55e26ab661745a6e04de569a2846372a054e8fe5
danilosheen <c.danilo.f.silva@gmail.com>
2022-12-26 15:17:00 -0300

    venv commands

---
+++ python_intermediario/commands_venv.py
@@ -0,0 +1,5 @@
+# pip freeze - mostra as versões das libs instaladas no ambiente virtual ou global
+
+# pip install -r nome-do-arquivo.txt - instala as versões do requirements
+
+# python -m venv venv - cria um ambiente virtual do projeto
